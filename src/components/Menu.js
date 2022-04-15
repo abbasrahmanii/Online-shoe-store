7d9fd3c91bf2d998255b3c02bfaee2f59a3c2d4c
abbasrahmanii <absrn26@gmail.com>
2022-04-15 19:05:23 +0430

    add active color to menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { Store } from "../context/Store";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
@@ -8,11 +8,17 @@ const Menu = () => {
   const { state, dispatch } = useContext(Store);
   const { cityData, menuStatus } = state;
 
+  const { pathname } = useLocation();
+
   const menuStyle = {
     zIndex: 100,
     left: menuStatus ? 0 : "-110%",
     transform: menuStatus ? "skew(0deg, 0deg)" : "skew(0deg, 15deg)",
     transition: "all 0.5s ease-out",
+  };
+
+  const activeMenuStyle = {
+    color: "#0fefff",
   };
 
   return (
@@ -23,56 +29,81 @@ const Menu = () => {
     >
       <ul className="divide-y divide-cyan-400 gap-6 flex flex-col">
         <li
-          className="text-slate-100 text-lg cursor-pointer"
+          className="text-slate-100 text-lg cursor-pointer hover:text-rose-500"
           onClick={() => dispatch({ type: "MENU_TOGGLE" })}
         >
           <FontAwesomeIcon icon={faCircleXmark} />
         </li>
-        <li className="text-slate-100 text-lg">
+        <li>
           <Link
             to="/"
             onClick={() => dispatch({ type: "MENU_TOGGLE" })}
             className="w-full"
           >
-            <h1 className="text-white">Home</h1>
+            <h1
+              className="text-slate-100 text-lg"
+              style={pathname === "/" ? activeMenuStyle : null}
+            >
+              Home
+            </h1>
           </Link>
         </li>
         {cityData.map((data) => (
-          <li className="text-slate-100 text-lg" key={data.id}>
+          <li key={data.id}>
             <Link
               to={data.url}
               onClick={() => dispatch({ type: "MENU_TOGGLE" })}
               className="w-full"
             >
-              <h1>{data.name}</h1>
+              <h1
+                className="text-slate-100 text-lg"
+                style={pathname === data.url ? activeMenuStyle : null}
+              >
+                {data.name}
+              </h1>
             </Link>
           </li>
         ))}
-        <li className="text-slate-100 text-lg">
+        <li>
           <Link
             to="/shop"
             onClick={() => dispatch({ type: "MENU_TOGGLE" })}
             className="w-full"
           >
-            <h1>Shop</h1>
+            <h1
+              className="text-slate-100"
+              style={pathname === "/shop" ? activeMenuStyle : null}
+            >
+              Shop
+            </h1>
           </Link>
         </li>
-        <li className="text-slate-100 text-lg">
+        <li>
           <Link
             to="/cart"
             onClick={() => dispatch({ type: "MENU_TOGGLE" })}
             className="w-full"
           >
-            <h1>Cart</h1>
+            <h1
+              className="text-slate-100"
+              style={pathname === "/cart" ? activeMenuStyle : null}
+            >
+              Cart
+            </h1>
           </Link>
         </li>
-        <li className="text-slate-100 text-lg">
+        <li>
           <Link
             to="/form"
             onClick={() => dispatch({ type: "MENU_TOGGLE" })}
             className="w-full"
           >
-            <h1>Form</h1>
+            <h1
+              className="text-slate-100"
+              style={pathname === "/form" ? activeMenuStyle : null}
+            >
+              Form
+            </h1>
           </Link>
         </li>
       </ul>
